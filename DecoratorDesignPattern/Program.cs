37ec7a4d7ade01ef6de096b97f5b2339cd204b59
Dademu <a00278997@mycambrian.ca>
2024-03-26 21:35:33 -0400

    Progarm.cs updated

--- DecoratorDesignPattern/Program.cs
+++ DecoratorDesignPattern/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DecoratorDesignPattern.Components;
+using DecoratorDesignPattern.Decorators;
+using DecoratorDesignPattern.Interfaces;
+using System;
+
+namespace DecoratorDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create ConcreteComponent
+            var concreteComponent = new ConcreteComponent();
+
+            // Create decorators
+            var plainDecorator = new PlainDecorators(concreteComponent);
+            var upperCaseDecorator = new UpperCaseDecorator(concreteComponent);
+            var colorDecorator = new ColorDecorator(concreteComponent);
+
+            // Call GetText() method on each object and print the result to the console
+            Console.WriteLine("Plain Decorator: " + plainDecorator.GetText());
+            Console.WriteLine("Upper Case Decorator: " + upperCaseDecorator.GetText());
+            Console.WriteLine("Color Decorator: " + colorDecorator.GetText());
+        }
+    }
+}
+
